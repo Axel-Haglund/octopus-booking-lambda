89f352e3b75de85527635351e4a7e973a7d507a8
Axel Haglund <axelhaglund@gmail.com>
2023-05-10 10:29:39 +0200

    working semi fixed

--- insert_bookings.php
+++ insert_bookings.php
@@ -2,35 +2,35 @@
 require("db-connection.php");
 session_start();
 
-if ($_SESSION["isLoggedIn"] == false) {
-    insertLoggedIn();
-} else {
-    insertRoom();
-}
+// if ($_SESSION["isLoggedIn"] == false) {
+//     insertLoggedIn();
+// } else {
+//     insertRoom();
+// }
 
-function insertLoggedIn()
-{
-    require("db-connection.php");
-    session_start();
+// function insertLoggedIn()
+// {
+require("db-connection.php");
+session_start();
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-        echo $_POST["email"];
+    echo $_POST["email"];
 
-        $json = file_get_contents('php://input');
-        $bookings = json_decode($json, true);
-        foreach ($bookings as $booking) {
+    $json = file_get_contents('php://input');
+    $bookings = json_decode($json, true);
+    foreach ($bookings as $booking) {
 
-            echo  var_dump($booking);
-            $date = $_SESSION["selectedDate"];
-            $user_id = $_SESSION["loggedInMember"]["user_id"];
-            $query = "INSERT INTO meeting (room_id, user_id, hour, date) Values ('{$booking["room"]}','{$user_id}','{$booking["hour"]}', '{$date}');";
-            echo $query;
-            mysqli_query($connection, $query);
-        }
-        echo 'Data received successfully';
+        echo  var_dump($booking);
+        $date = $_SESSION["selectedDate"];
+        $user_id = $_SESSION["loggedInMember"]["user_id"];
+        $query = "INSERT INTO meeting (room_id, user_id, hour, date) Values ('{$booking["room"]}','{$user_id}','{$booking["hour"]}', '{$date}');";
+        echo $query;
+        mysqli_query($connection, $query);
     }
+    echo 'Data received successfully';
 }
+// }
 
 function insertRoom()
 {
